bc8d5fef46ce024de2a51da4fd2cdc14a072ebe7
Tom Veldhuis <t.veldhuis@student.rug.nl>
2019-05-27 13:47:12 +0200

    added use of SpaCy library

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # Main program
 
-import sys
+import sys, spacy
 import syntacticAnalysis as s
 import wikidataQuery as q
 
 def main(argv):
-	print("State a question:");
+	print("Loading SpaCy library...")
+	# nlp = spacy.load('en')
+	print("State a question:")
 	for line in sys.stdin:
 		line = line.rstrip()
 		# Input here the process for answering the question
-		s.syntacticAnalysis()
+		s.syntacticAnalysis(nlp, line)
 		q.makeQuery()
 		print("State a question:")
 
