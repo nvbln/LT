c73aac27f2d0b5a325d95197c55b57f50af6d6f5
Nathan van Beelen <nathan@vanbeelen.org>
2019-05-28 11:56:28 +0200

    Add commandline argument functionality

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # Main program
 
-import sys, spacy
+import getopt, sys, spacy
 import syntacticAnalysis as s
 import wikidataQuery as q
 import csv
+
+def printHelp():
 
 def testQuestions():
 	with open("all_questions_and_answers.tsv") as tsvfile:
@@ -16,6 +18,30 @@ def testQuestions():
 					print(" - " + x)
 
 def main(argv):
+    # Check commandline arguments
+    full_cmd_arguments = argv
+    argument_list = full_cmd_arguments[1:]
+    
+    # Argument options: help, test
+    unix_options = "ht"
+    gnu_options = ["help", "test"]
+
+    try:
+        arguments, values = getopt.getopt(argument_list, unix_options, gnu_options)
+    except getopt.error as err:
+        # output error, and return with an error code
+        print (str(err))
+        sys.exit(2)
+
+    # Evaluate given options
+    for current_argument, current_value in arguments:
+        if current_argument in ("-h", "--help"):
+            printHelp()
+            exit()
+        elif current_argument in ("-t", "--test"):
+            evaluateTestQuestions()
+            exit()
+
 	print("Loading SpaCy library...")
 	# nlp = spacy.load('en')
 	testQuestions()
