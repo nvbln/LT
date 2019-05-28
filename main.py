1011929de98a5ba291e025aea91c720862c76849
Tom Veldhuis <t.veldhuis@student.rug.nl>
2019-05-28 10:12:41 +0200

    added testQuestions() to main.py, for checking the answer of the test questions

--- main.py
+++ main.py
@@ -3,10 +3,22 @@
 import sys, spacy
 import syntacticAnalysis as s
 import wikidataQuery as q
+import csv
+
+def testQuestions():
+	with open("all_questions_and_answers.tsv") as tsvfile:
+		tsvreader = csv.reader(tsvfile, delimiter="\t")
+		for line in tsvreader:
+			for x in line:
+				if x == line[0]:
+					print(x)
+				if x != line[0] and x != line[1]:
+					print(" - " + x)
 
 def main(argv):
 	print("Loading SpaCy library...")
 	# nlp = spacy.load('en')
+	testQuestions()
 	print("State a question:")
 	for line in sys.stdin:
 		line = line.rstrip()
@@ -17,3 +29,4 @@ def main(argv):
 
 if __name__ == "__main__":
 	main(sys.argv)
+	
