9d5148b295758a101c2099ccfbc78114cd4e256d
Tom Veldhuis <t.veldhuis@student.rug.nl>
2019-06-08 13:10:52 +0200

    added convertPOS-function for changing the POS of a word into another POS (using WordNet)

--- syntacticAnalysis.py
+++ syntacticAnalysis.py
@@ -1,5 +1,13 @@
 # Functions needed for syntactic analysis
 import settings
+from nltk.corpus import wordnet as wn
+ 
+# Static variables for the convertPOS-function
+WN_NOUN = 'n'
+WN_VERB = 'v'
+WN_ADJECTIVE = 'a'
+WN_ADJECTIVE_SATELLITE = 's'
+WN_ADVERB = 'r'
 
 # A pre-defined dictionary for difficult terms
 key_words_dict = {'band members': 'has part', 'members': 'has part',
@@ -166,3 +174,45 @@ def sentenceContains(sentence, keyword, start_position):
 
     # The keyword could not be found.
     return -1
+
+# Converts a word string from POS to another POS, using WordNet
+# WN_NOUN: noun
+# WN_VERB: verb
+# WN_ADJECTIVE: adjective
+# WN_ADJECTIVE_SATELLITE: ?
+# WN_ADVERB: adverb
+def convertPOS(word, from_pos, to_pos):
+    synsets = wn.synsets(word, pos=from_pos)
+ 
+    # Word not found
+    if not synsets:
+        return []
+ 
+    # Get all lemmas of the word (consider 'a'and 's' equivalent)
+    lemmas = [l for s in synsets
+                for l in s.lemmas() 
+                if s.name().split('.')[1] == from_pos
+                    or from_pos in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE)
+                        and s.name().split('.')[1] in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE)]
+ 
+    # Get related forms
+    derivationally_related_forms = [(l, l.derivationally_related_forms()) for l in lemmas]
+ 
+    # filter only the desired pos (consider 'a' and 's' equivalent)
+    related_noun_lemmas = [l for drf in derivationally_related_forms
+                             for l in drf[1] 
+                             if l.synset().name().split('.')[1] == to_pos
+                                or to_pos in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE)
+                                    and l.synset().name().split('.')[1] in (WN_ADJECTIVE, WN_ADJECTIVE_SATELLITE)]
+ 
+    # Extract the words from the lemmas
+    words = [l.name() for l in related_noun_lemmas]
+    len_words = len(words)
+ 
+    # Build the result in the form of a list containing tuples (word, probability)
+    result = [(w, float(words.count(w))/len_words) for w in set(words)]
+    result.sort(key=lambda w: -w[1])
+ 
+    # return all the possibilities sorted by probability
+    # for each element e in result, e[0] gives the name and e[1] gives the probability
+    return result
