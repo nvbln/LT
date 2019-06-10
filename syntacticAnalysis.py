84c783ddd31ce6b2a6a1444afc44da6e6919a51c
Nathan van Beelen <nathan@vanbeelen.org>
2019-06-10 15:14:47 +0200

    Catch and replace names in the question

--- syntacticAnalysis.py
+++ syntacticAnalysis.py
@@ -15,6 +15,24 @@ def syntacticAnalysis(nlp, line):
 
     keywords = {}
 
+    names = []
+
+    # Retrieve the names from the question.
+    new_line = line
+    for word in question:
+        # It's the start of a new name.
+        if word.ent_iob_ == "B":
+            names.append(word.text)
+        # It's the continuation of a name.
+        elif word.ent_iob_ == "I":
+            names[-1] += " " + word.text
+            new_line = new_line.replace(" " + word.text, word.text, 1)
+    question = nlp(new_line)
+
+    if settings.verbose:
+        print("Question after name replacement:")
+        print(question)
+
     # Check if the sentence contains a date (and remove it).
     # TODO: Clean up code.
     # TODO: Check if the date is not between puncts. This likely means that
